369ef6768a4230094d50d8f01bd15593eb3fe61f
Eben Sorkin <sorkineben@gmail.com>
2019-12-19 15:38:27 -0500

    new UFO files

--- sources/build.sh
+++ sources/build.sh
@@ -4,10 +4,10 @@ set -e
 
 cd sources
 
-#echo "Generating Static fonts"
-#mkdir -p ../fonts/ttfs
-#fontmake -g Merriweather-Roman.glyphs -i -o ttf --output-dir ../fonts/ttfs/
-#fontmake -g Merriweather-Italic.glyphs -i -o ttf --output-dir ../fonts/ttfs/
+echo "Generating Static fonts"
+mkdir -p ../fonts/ttfs
+fontmake -g Merriweather-Roman.glyphs -i -o ttf --output-dir ../fonts/ttfs/
+fontmake -g Merriweather-Italic.glyphs -i -o ttf --output-dir ../fonts/ttfs/
 
 echo "Generating VFs"
 mkdir -p ../fonts/variable
