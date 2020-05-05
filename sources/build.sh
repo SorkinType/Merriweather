256246c22c4f488246ac04fb7ac0597c786f4597
Eben Sorkin <sorkineben@gmail.com>
2020-05-04 20:23:22 -0400

    Revert "Updated build & sources"
    This reverts commit 04bad663a40aae18c7479da2d466855ca1507365.

--- sources/build.sh
+++ sources/build.sh
@@ -6,8 +6,8 @@ cd sources
 
 echo "Generating Static fonts"
 mkdir -p ../fonts/ttfs
-fontmake -g Merriweather-Roman.glyphs -i -o ttf --output-dir ../fonts/ttfs/
-fontmake -g Merriweather-Italic.glyphs -i -o ttf --output-dir ../fonts/ttfs/
+#fontmake -g Merriweather-Roman.glyphs -i -o ttf --output-dir ../fonts/ttfs/
+#fontmake -g Merriweather-Italic.glyphs -i -o ttf --output-dir ../fonts/ttfs/
 
 echo "Generating VFs"
 mkdir -p ../fonts/variable
